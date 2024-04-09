53c3d17e56860c1d79ecdac50bfa54de42746e6e
Briland Mathieu <briland.mathieu@gmail.com>
2024-04-09 23:47:48 +0200

    change pullsecret and helloworld2 go to 0.2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World 2');
 });
 
 // Step 5: Start the server
